d4930b77903f03a2928e686211cd32dd3c69dbc1
Wilson <wilsonoficial.com@gmail.com>
2025-03-24 14:38:20 -0300

    criado uma interface de formulario

---
+++ app/interfaces/interfaceForm.ts
@@ -0,0 +1,11 @@
+interface Field {
+    name: string;
+    label: string;
+    className?: string;
+    type: "text" | "email" | "password" | "number" | "date" | "select" | "textarea" | "select";
+    options?: {value: string, label: string, className: string}[];
+    placeholder?: string;
+    required?: boolean;
+}
+
+export default Field;
