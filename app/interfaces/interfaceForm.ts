f2fb5c94c9c4958f0eb82755ce8388a41f875d58
Wilson <wilsonoficial.com@gmail.com>
2025-03-24 16:22:37 -0300

    foram adicionadas duas novos propriedades na interface e uma propriedade ja existente modificada

--- app/interfaces/interfaceForm.ts
+++ app/interfaces/interfaceForm.ts
@@ -1,9 +1,11 @@
 interface Field {
     name: string;
-    label: string;
+    label?: string;
     className?: string;
     type: "text" | "email" | "password" | "number" | "date" | "select" | "textarea" | "select";
     options?: {value: string, label: string, className?: string}[];
+    classNameInput?: string;
+    classNameLabel?: string;
     placeholder?: string;
     required?: boolean;
 }
