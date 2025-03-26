14f43cb772812522b4822f84cb9b3c4dc555188f
Wilson <wilsonoficial.com@gmail.com>
2025-03-26 15:29:08 -0300

    redirecionando o usuário para o dashboard ao acessar a página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Dashboard from "./components/Dashboard/Dashboard";
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(()=> {
+    router.push("/dashboard");
+  },[router]);
+
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <Dashboard />
+     
     </div>
   );
 }
