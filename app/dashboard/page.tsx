f14bd4781b1f8011a322c8276b41cc33133411b6
Wilson <wilsonoficial.com@gmail.com>
2025-03-26 15:29:55 -0300

    adicionado componente de dashboard à página inicial

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Dashboard from "../components/Dashboard/Dashboard";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Dashboard />
+    </div>
+  );
+}
