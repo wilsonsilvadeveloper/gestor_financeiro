5148c9e25b40d88d534be0cb52063f4c021d5875
Wilson <wilsonoficial.com@gmail.com>
2025-03-28 10:44:20 -0300

    adicionado interface AnimationProps para configuração de animações

---
+++ app/interface/animation.ts
@@ -0,0 +1,8 @@
+export default interface AnimationProps {
+    data: string;
+    className?: string;
+    loop?: boolean;
+    autoplay?: boolean;
+    width?: number | string;
+    height?: number | string;
+}
