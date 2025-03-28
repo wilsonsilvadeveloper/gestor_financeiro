7fe08d4ea528e436c491e2f950346ae8f816202b
Wilson <wilsonoficial.com@gmail.com>
2025-03-28 10:15:10 -0300

    adicionado tratamento de erro e carregamento de dados no componente de dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,53 @@
+"use client";
+import { useEffect, useState } from "react";
 import Dashboard from "../components/Dashboard/Dashboard";
+import Error from "../components/Error/Error";
+import { ErrorRequest } from "../interface/error";
 
 export default function Home() {
+  const [data, setData] = useState<object | null>(null);
+  const [error, setError] = useState<ErrorRequest | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchDataDashboard = async () => {
+      try {
+        const response = await fetch("http://localhost:3001/api/users/dashboard", {
+          method: "GET",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+        });
+
+        if (response.status === 401) {
+          const errorData = await response.json();
+          setError({ status: response.status, message: errorData.message });
+        } else {
+          const jsonData = await response.json();
+          setData(jsonData);
+        }
+      } catch (err) {
+        console.error("Erro ao buscar dados:", err);
+        setError({ status: 500, message: "Erro ao buscar dados." });
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchDataDashboard();
+  }, []);
+
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <Dashboard />
+      {loading ? (
+        <p>Carregando...</p>
+      ) : error ? (
+        <Error status={error.status} message={error.message} />
+      ) : (
+        <Dashboard />
+      )}
     </div>
   );
 }
