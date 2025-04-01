b36485fa53c026f73453a19fcbb1eb8b75a6ed05
Wilson <wilsonoficial.com@gmail.com>
2025-04-01 16:20:31 -0300

    adicionado componente SideBar à página do dashboard e atualizado a lógica de renderização para incluir dados do dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,53 +1,66 @@
-"use client";
-import { useEffect, useState } from "react";
-import Dashboard from "../components/Dashboard/Dashboard";
-import Error from "../components/Error/Error";
-import { ErrorRequest } from "../interface/error";
+  "use client";
+  import { useEffect, useState } from "react";
+  import Dashboard from "../components/Dashboard/Dashboard";
+  import Error from "../components/Error/Error";
+  import { ErrorRequest } from "../interface/error";
+  import { DashboardData } from "../interface/dashboard";
+  import SideBar from "../components/SideBar/SideBar";
 
-export default function Home() {
-  const [data, setData] = useState<object | null>(null);
-  const [error, setError] = useState<ErrorRequest | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
+  export default function Home() {
+    const [data, setData] = useState<DashboardData>();
+    const [error, setError] = useState<ErrorRequest | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    const fetchDataDashboard = async () => {
-      try {
-        const response = await fetch("http://localhost:3001/api/users/dashboard", {
-          method: "GET",
-          credentials: "include",
-          headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json",
-          },
-        });
+    useEffect(() => {
+      const fetchDataDashboard = async () => {
+        try {
+          const response = await fetch("http://localhost:3001/api/users/dashboard", {
+            method: "GET",
+            credentials: "include",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json",
+            },
+          });
 
-        if (response.status === 401) {
-          const errorData = await response.json();
-          setError({ status: response.status, message: errorData.message });
-        } else {
-          const jsonData = await response.json();
-          setData(jsonData);
+          if (response.status === 401) {
+            const errorData = await response.json();
+            setError({ status: response.status, message: errorData.message });
+          } else {
+            const jsonData = await response.json();
+            const dataExtrato: DashboardData = {
+                receitas: jsonData.receitas as number[],
+                gastos: jsonData.gastos as number[],
+                labels: jsonData.labels as string[],
+            }
+            console.log("Dados do dashboard:", dataExtrato);
+            setData(dataExtrato);
+          }
+        } catch (err) {
+          console.error("Erro ao buscar dados:", err);
+          setError({ status: 500, message: "Erro ao buscar dados." });
+        } finally {
+          setLoading(false);
         }
-      } catch (err) {
-        console.error("Erro ao buscar dados:", err);
-        setError({ status: 500, message: "Erro ao buscar dados." });
-      } finally {
-        setLoading(false);
-      }
-    };
+      };
 
-    fetchDataDashboard();
-  }, []);
+      fetchDataDashboard();
+    }, []);
 
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      {loading ? (
-        <p>Carregando...</p>
-      ) : error ? (
-        <Error status={error.status} message={error.message} />
-      ) : (
-        <Dashboard />
-      )}
-    </div>
-  );
-}
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-white">
+        {loading ? (
+          <p>Carregando...</p>
+        ) : error ? (
+          <Error status={error.status} message={error.message} />
+        ) : (
+          <div className="flex flex-row gap-4 w-full min-h-screen">
+            <SideBar />
+            <div className="flex items-center justify-center w-full h-screen">
+              <Dashboard data={data} />
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
