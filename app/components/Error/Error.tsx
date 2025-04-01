d11272371afa797b13c7a3f90f1fd3e4b6453f69
Wilson <wilsonoficial.com@gmail.com>
2025-04-01 16:18:55 -0300

    atualizado componente Error para exibir mensagem em tamanho maior e corrigido redirecionamento para a página de login

--- app/components/Error/Error.tsx
+++ app/components/Error/Error.tsx
@@ -14,11 +14,11 @@ export default function Error(erorInfo: ErrorRequest) {
     return (
         <div className="w-full flex flex-col items-center justify-center min-h-screen text-red-600">
             <h1 className="text-4xl font-bold">Error {status}</h1>
-            <p className="mt-4 font-medium">{message}</p>
+            <p className="mt-4 font-medium text-2xl">{message}</p>
             <Button label="Faça Login"
                 disabled={false}
                 type="button"
-                onClick={()=> router.push("/create-account")}
+                onClick={()=> router.push("/login")}
                 className="flex justify-center items-center absolute bottom-[40px] w-[200px] h-[40px] text-white rounded-lg bg-gray-950 cursor-pointer"
             />
         </div>
