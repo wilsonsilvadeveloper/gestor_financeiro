c4293ea07ac73d6625661a45baed5eadd7f98952
Wilson <wilsonoficial.com@gmail.com>
2025-04-01 16:13:08 -0300

    refatorado componente Button para desestruturar props e permitir uso de children

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,13 +1,9 @@
-interface ButtonProps {
-    label: string;
-    onClick?: () => void;
-    disabled?: boolean;
-    className?: string;
-    type?: "button" | "submit" | "reset";
-}
+    import ButtonProps from "@/app/interface/button";
 
-export default function Button(ButtonProps: ButtonProps){
-    return (
-        <button type={ButtonProps.type} disabled={ButtonProps.disabled} className={ButtonProps.className} onClick={ButtonProps.onClick}>{ButtonProps.label}</button>
-    )
-}
+    export default function Button({ label, onClick, disabled, className, type, children }: ButtonProps) {
+        return (
+            <button type={type} disabled={disabled} className={className} onClick={onClick}>
+                {children ? children : label}
+            </button>
+        );
+    }
