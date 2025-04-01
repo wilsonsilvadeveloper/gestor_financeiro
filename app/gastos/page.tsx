ee09849631b9623314e88f7fd75a746d1e3a77fb
Wilson <wilsonoficial.com@gmail.com>
2025-04-01 16:20:11 -0300

    criado paginas de Gastos e Receitas para gerenciamento de despesas e receitas

---
+++ app/gastos/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Gastos() {
+    return (
+        <div className="flex flex-col w-full h-screen">
+            <div className="flex flex-col w-full h-full items-center justify-center">
+                <h1 className="text-3xl font-bold">Gastos</h1>
+                <p className="text-gray-500">Aqui você pode gerenciar seus gastos.</p>
+            </div>
+        </div>
+    )
+}
