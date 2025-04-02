e6311cb7e715b9350b02008ab844e15fffcfdcd9
Wilson <wilsonoficial.com@gmail.com>
2025-04-02 16:23:31 -0300

    adicionado lógica de validação de dados e estrutura de layout ao componente Receitas

--- app/receitas/page.tsx
+++ app/receitas/page.tsx
@@ -1,11 +1,50 @@
+"use client";
+import { useEffect, useState } from "react";
+import Button from "../components/Button/Button";
+import Header from "../components/Header/Header";
+import SideBar from "../components/SideBar/SideBar";
 
-export default function Receitas() {
+interface ReceitasProps {
+    data: {
+        receitas: number[];
+        labels: string[];
+        categorias: string[];
+    }
+}
+
+export default function Receitas({ data } : ReceitasProps) {
+
+    const [dataIsValid, setDataIsValid] = useState(false);
+
+    useEffect(()=> {
+        if (data && data.receitas && data.labels && data.categorias) {
+            setDataIsValid(true);
+        } else {
+            setDataIsValid(false);
+        }
+    })
+
     return (
-        <div className="flex flex-col w-full h-screen">
-            <div className="flex flex-col w-full h-full items-center justify-center">
-                <h1 className="text-3xl font-bold">Receitas</h1>
-                <p className="text-gray-500">Aqui você pode gerenciar suas receitas.</p>
-            </div>
+        <div className="flex flex-row w-full h-screen bg-white">
+            <SideBar />
+            <section className="flex flex-col w-full">
+                <Header className="relative top-0 left-0 w-full h-16 flex items-center justify-between px-4">
+                    <h1 className="text-2xl font-bold text-[#ff9800]">Receitas</h1>
+                    <Button label="Nova Receita" className="bg-[#ff9800] w-[150px] h-10 rounded-lg cursor-pointer"/>
+                </Header>
+                <main>
+                    {dataIsValid ? (
+                        <div>
+                            <h1>Dados validados</h1>
+                        </div>
+                    ) : (
+                        <div className="flex flex-col items-center justify-center w-full text-black">
+                            <h1>Dados inválidos</h1>
+                            <p>Verifique se os dados estão corretos.</p>
+                        </div>
+                    )}
+                </main>
+            </section>
         </div>
     )
 }
